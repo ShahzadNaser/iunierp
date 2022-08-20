5b044fe20ae89193a339f863dad9e5eb49a24174
erpdev <erpdev@iuni.com.vn>
2022-08-20 01:40:20 +0000

    updated old doctypes and moved theminto custom app

---
+++ iunierp/iunierp/doctype/iuni_department/iuni_department.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Shahzad Naser and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class IUNIDepartment(NestedSet):
+	pass
---
+++ iunierp/iunierp/doctype/iuni_test_contract_master_input/iuni_test_contract_master_input.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Shahzad Naser and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('IUNI TEST Contract Master Input', {
+	// refresh: function(frm) {
+
+	// }
+});
